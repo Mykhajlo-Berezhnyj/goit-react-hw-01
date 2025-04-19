fc5b5b68584b323c298398304b46373fc2d6e6c9
Mykhajlo <bmve@ukr.net>
2025-04-19 20:52:40 +0300

    add styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,16 +2,16 @@ import TransactionElement from '../TransactionElement/TransactionElement';
 import css from './TransactionHistory.module.css';
 
 const TransactionHistory = ({items}) => {
-    return (<table>
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
+    return (<table className={css.table}>
+        <thead className={css.thead}>
+            <tr className={css.tr}>
+                <th className={css.th}>Type</th>
+                <th className={css.th}>Amount</th>
+                <th className={css.th}>Currency</th>
             </tr>
         </thead>
 
-        <tbody>
+        <tbody className={css.tbody}>
             {items.map((transaction) => (
                 <TransactionElement
                     key = {transaction.id}
